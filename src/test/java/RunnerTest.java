c13ffd49216dd383e0a6d82014a781bb27f65494
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2017-04-18 18:01:21 -0700

    create a test file for the Runner
    - includes a trivial test to make sure things are working

---
+++ src/test/java/RunnerTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ErinGreenhalgh on 4/18/17.
+ */
+public class RunnerTest {
+
+    @Test
+    public void trivialTest() {
+        boolean theTruth = true;
+        assertTrue(theTruth);
+    }
+}
