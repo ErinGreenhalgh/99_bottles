f62586f5c12204f4b37d2943406b1c063fddc7bc
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2017-05-03 16:51:32 -0600

    unignore testVerseOne and make it pass
    - add more if / else checks that determine structure
    of the first and last lines if the number given to
    the method is 1 and if the number minus one is 0
    - lots of repeated code! think about a method the formats
    the line so each string fragment doesn’t have to be
    used multiple times

--- src/main/java/Bottles.java
+++ src/main/java/Bottles.java
@@ -9,12 +9,20 @@ public class Bottles {
 
     String verse(int numberOfBottles) {
         String number = Integer.toString(numberOfBottles);
-        String firstLine = number + " " + bottlex + plural + " " + ofBeer + " " + onWall + ", " + number + " " + bottlex + plural + " " + ofBeer + ".\n";
+        String firstLine;
+        if (numberOfBottles == 1) {
+            firstLine = number + " " + bottlex + " " + ofBeer + " " + onWall + ", " + number + " " + bottlex + " " + ofBeer + ".\n";
+        } else {
+            firstLine = number + " " + bottlex + plural + " " + ofBeer + " " + onWall + ", " + number + " " + bottlex + plural + " " + ofBeer + ".\n";
+        }
+
 
         int numberMinusOne = numberOfBottles - 1;
         String lastLine;
         if (numberMinusOne == 1) {
             lastLine = refrain + ", " + Integer.toString(numberMinusOne) + " " + bottlex + " " + ofBeer +" " + onWall + ".";
+        } else if (numberMinusOne == 0) {
+            lastLine = "Take it down and pass it around" + ", " + noMore + " " + bottlex + plural + " " + ofBeer + " "+ onWall + ".";
         } else {
             lastLine = refrain + ", " + Integer.toString(numberMinusOne) + " " + bottlex + plural + " " + ofBeer + " "+ onWall + ".";
         }
--- src/test/java/BottlesTest.java
+++ src/test/java/BottlesTest.java
@@ -31,7 +31,7 @@ public class BottlesTest {
         assertEquals(verse, expectedVerse);
     }
 
-    @Ignore @Test
+    @Test
     public void testVerseOne() {
         String verse = "1 bottle of beer on the wall, 1 bottle of beer.\n" +
                 "Take it down and pass it around, no more bottles of beer on the wall.";
