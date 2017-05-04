d3e18b7fe1d85679cf2aafed65b09a4c0c09cc95
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2017-05-03 18:01:51 -0600

    work on making tests for multiple verses pass
    -modify verse method to take two int args, a starting
    number and an ending number
    - add a while loop that formats the first line, 
    subtracts one from the number, and formats the last 
    line of the verse based on that number
    - utilize method overloading to allow just one
    arg to be given to the verse method;if only
    one arg is given, the two args of the verse method
    are both the starting number, ensuring that the
    while loop will only run once 
    - add a String song that collects the lines as they 
    are created, adding new lines and double new lines 
    between verses. This is working for two verses but 
    the newlines are messed up for more than two verses

--- src/main/java/Bottles.java
+++ src/main/java/Bottles.java
@@ -7,31 +7,53 @@ public class Bottles {
     String noMore = "no more";
     String refrain = "Take one down and pass it around";
 
-    String verse(int numberOfBottles) {
-        String number = Integer.toString(numberOfBottles);
-        String firstLine;
-        if (numberOfBottles == 1) {
-            firstLine = number + " " + bottlex + " " + ofBeer + " " + onWall + ", " + number + " " + bottlex + " " + ofBeer + ".\n";
-        } else if (numberOfBottles == 0) {
-            firstLine = noMore.substring(0,1).toUpperCase() + noMore.substring(1) + " " + bottlex + plural + " " + ofBeer + " " + onWall + ", " + noMore + " " + bottlex + plural + " " + ofBeer + ".\n";
-        }
-        else {
-            firstLine = number + " " + bottlex + plural + " " + ofBeer + " " + onWall + ", " + number + " " + bottlex + plural + " " + ofBeer + ".\n";
-        }
+    String verse(int startingNumber, int endingNumber) {
+        int number = startingNumber;
+        String song = "";
+        while (number >= endingNumber) {
+            //format the first line based on the number
+            String num = Integer.toString(number);
+            String firstLine;
+            if (number == 1) {
+                firstLine = num + " " + bottlex + " " + ofBeer + " " + onWall + ", " + num + " " + bottlex + " " + ofBeer + ".\n";
+            } else if (number == 0) {
+                firstLine = noMore.substring(0,1).toUpperCase() + noMore.substring(1) + " " + bottlex + plural + " " + ofBeer + " " + onWall + ", " + noMore + " " + bottlex + plural + " " + ofBeer + ".\n";
+            }
+            else {
+                firstLine = num + " " + bottlex + plural + " " + ofBeer + " " + onWall + ", " + num + " " + bottlex + plural + " " + ofBeer + ".\n";
+            }
+            song = song + firstLine;
+
+            //subtract one from the number
+            number = number - 1;
+
+            //format the second line based on that number
+            String lastLine;
+            if (number == 1) {
+                lastLine = refrain + ", " + Integer.toString(number) + " " + bottlex + " " + ofBeer +" " + onWall + ".";
+            } else if (number == 0) {
+                lastLine = "Take it down and pass it around" + ", " + noMore + " " + bottlex + plural + " " + ofBeer + " "+ onWall + ".";
+            } else if (number < 0) {
+                lastLine = "Go to the store and buy some more, 99" + " " + bottlex + plural + " " + ofBeer + " "+ onWall + ".";
+            } else {
+                lastLine = refrain + ", " + Integer.toString(number) + " " + bottlex + plural + " " + ofBeer + " "+ onWall + ".";
+            }
+
+            //the song always has a double new line between the second line and the new first line
+            //but has no double new line at the end
+            if (number == endingNumber) {
+                song = song + lastLine + "\n\n";
+            } else {
+                song = song + lastLine;
+            }
 
 
-        int numberMinusOne = numberOfBottles - 1;
-        String lastLine;
-        if (numberMinusOne == 1) {
-            lastLine = refrain + ", " + Integer.toString(numberMinusOne) + " " + bottlex + " " + ofBeer +" " + onWall + ".";
-        } else if (numberMinusOne == 0) {
-            lastLine = "Take it down and pass it around" + ", " + noMore + " " + bottlex + plural + " " + ofBeer + " "+ onWall + ".";
-        } else if (numberMinusOne < 0) {
-            lastLine = "Go to the store and buy some more, 99" + " " + bottlex + plural + " " + ofBeer + " "+ onWall + ".";
-        } else {
-            lastLine = refrain + ", " + Integer.toString(numberMinusOne) + " " + bottlex + plural + " " + ofBeer + " "+ onWall + ".";
         }
 
-        return firstLine + lastLine;
+        return song;
+    }
+
+    String verse(int startingNumber) {
+        return verse(startingNumber, startingNumber);
     }
 }
--- src/test/java/BottlesTest.java
+++ src/test/java/BottlesTest.java
@@ -49,29 +49,29 @@ public class BottlesTest {
         assertEquals(verse, expectedVerse);
     }
 
-//    @Ignore @Test
-//    public void testACoupleOfVerses() {
-//        String verses = "99 bottles of beer on the wall, 99 bottles of beer.\n" +
-//                "Take one down and pass it around, 98 bottles of beer on the wall.\n\n" +
-//                "98 bottles of beer on the wall, 98 bottles of beer.\n" +
-//                "Take one down and pass it around, 97 bottles of beer on the wall.";
-//        Bottles b = new Bottles();
-//        String expectedVerses = b.verse(99, 98);
-//        assertEquals(verses, expectedVerses);
-//    }
+    @Test
+    public void testACoupleOfVerses() {
+        String verses = "99 bottles of beer on the wall, 99 bottles of beer.\n" +
+                "Take one down and pass it around, 98 bottles of beer on the wall.\n\n" +
+                "98 bottles of beer on the wall, 98 bottles of beer.\n" +
+                "Take one down and pass it around, 97 bottles of beer on the wall.";
+        Bottles b = new Bottles();
+        String expectedVerses = b.verse(99, 98);
+        assertEquals(verses, expectedVerses);
+    }
 
-//    @Ignore @Test
-//    public void testMultipleVerses() {
-//        String verses = "2 bottles of beer on the wall, 2 bottles of beer.\n" +
-//                "Take one down and pass it around, 1 bottle of beer on the wall.\n\n" +
-//                "1 bottle of beer on the wall, 1 bottle of beer.\n" +
-//                "Take it down and pass it around, no more bottles of beer on the wall.\n\n"+
-//                "No more bottles of beer on the wall, no more bottles of beer.\n" +
-//                "Go to the store and buy some more, 99 bottles of beer on the wall.";
-//        Bottles b = new Bottles();
-//        String expectedVerses = b.verse(2,0);
-//        assertEquals(verses, expectedVerses);
-//    }
+    @Test
+    public void testMultipleVerses() {
+        String verses = "2 bottles of beer on the wall, 2 bottles of beer.\n" +
+                "Take one down and pass it around, 1 bottle of beer on the wall.\n\n" +
+                "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+                "Take it down and pass it around, no more bottles of beer on the wall.\n\n"+
+                "No more bottles of beer on the wall, no more bottles of beer.\n" +
+                "Go to the store and buy some more, 99 bottles of beer on the wall.";
+        Bottles b = new Bottles();
+        String expectedVerses = b.verse(2,0);
+        assertEquals(verses, expectedVerses);
+    }
 //
 //    @Ignore @Test
 //    public void testTheWholeThing() {
