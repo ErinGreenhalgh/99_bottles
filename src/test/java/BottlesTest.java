d9ce169486b79765452acc90f854255a1ed4ceba
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2017-05-15 10:21:55 -0600

    add a final test that tests several verses at once

--- src/test/java/BottlesTest.java
+++ src/test/java/BottlesTest.java
@@ -72,9 +72,24 @@ public class BottlesTest {
         String expectedVerses = b.verse(2,0);
         assertEquals(verses, expectedVerses);
     }
-//
-//    @Ignore @Test
-//    public void testTheWholeThing() {
-//
-//    }
+
+    @Test
+    public void testLotsOfVerses() {
+      String verses = "5 bottles of beer on the wall, 5 bottles of beer.\n" +
+              "Take one down and pass it around, 4 bottles of beer on the wall.\n\n" +
+              "4 bottles of beer on the wall, 4 bottles of beer.\n" +
+              "Take one down and pass it around, 3 bottles of beer on the wall.\n\n" +
+              "3 bottles of beer on the wall, 3 bottles of beer.\n" +
+              "Take one down and pass it around, 2 bottles of beer on the wall.\n\n" +
+              "2 bottles of beer on the wall, 2 bottles of beer.\n" +
+              "Take one down and pass it around, 1 bottle of beer on the wall.\n\n" +
+              "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+              "Take it down and pass it around, no more bottles of beer on the wall.\n\n"+
+              "No more bottles of beer on the wall, no more bottles of beer.\n" +
+              "Go to the store and buy some more, 99 bottles of beer on the wall.";
+
+      Bottles b = new Bottles();
+      String expectedVerses = b.verse(5, 0);
+      assertEquals(verses, expectedVerses);
+    }
 }
